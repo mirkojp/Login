8a7f963bef9a1b97426f00aa9cfc0e50e0efff27
UADER_IS2_PAVAN <127816065+mirkojp@users.noreply.github.com>
2024-11-13 18:43:18 -0300

    Add files via upload

---
+++ js/mercadoPago.js
@@ -0,0 +1,25 @@
+// Inicializa el SDK de MercadoPago con tu clave pública
+const mp = new MercadoPago('APP_USR-61f3d47d-4634-4a02-9185-68f2255e63c2'); // Reemplaza con tu clave pública
+
+// Obtén el preferenceId desde Django
+fetch('https://practica-django-fxpz.onrender.com/create_preference/')
+    .then(response => response.json())
+    .then(data => {
+        const preferenceId = data.preference_id;
+
+        // Carga el widget de Wallet con el preferenceId
+        mp.bricks().create("wallet", "wallet_container", {
+            initialization: {
+                preferenceId: preferenceId
+            },
+            customization: {
+                texts: {
+                    valueProp: 'smart_option',
+                },
+            },
+        }).then(() => {
+            console.log("Widget de Wallet cargado correctamente");
+        }).catch(error => {
+            console.error("Error al cargar el widget de Wallet:", error);
+        });
+    });
