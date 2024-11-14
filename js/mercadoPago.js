058cfba8c6ad3886ac91efeb22b029ae03d98348
mirkojp <mirkopavan88878@gmail.com>
2024-11-13 22:20:47 -0300

    update 22 20 13 11 24

--- js/mercadoPago.js
+++ js/mercadoPago.js
@@ -1,4 +1,22 @@
+function getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+
+const csrftoken = getCookie('csrftoken');
+
 // Inicializa el SDK de MercadoPago con tu clave pública
+
 const mp = new MercadoPago('APP_USR-61f3d47d-4634-4a02-9185-68f2255e63c2'); 
 
 // Obtén el preferenceId desde Django
@@ -6,6 +24,7 @@ fetch('https://practica-django-fxpz.onrender.com/create_preference/', {
     method: 'POST',
     headers: {
         'Content-Type': 'application/json',
+        'X-CSRFToken': csrftoken,
     },
 })
     .then(response => response.json())
